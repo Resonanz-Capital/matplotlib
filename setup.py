9ef274ea784735e4aa18517ad56f25ebccfd8b2f
vkolevrestec <viktor.kolev@restec.io>
2024-04-05 16:20:35 +0300

    Set version

--- setup.py
+++ setup.py
@@ -269,6 +269,7 @@ if not (any('--' + opt in sys.argv
 
 setup(  # Finally, pass this all along to setuptools to do the heavy lifting.
     name="matplotlib",
+    version='3.7.1',
     description="Python plotting package",
     author="John D. Hunter, Michael Droettboom",
     author_email="matplotlib-users@python.org",
